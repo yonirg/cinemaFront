253a3318f04ea176fb9587da0cee7711f77d8736
yonirg <yonirabinovicigherman@gmail.com>
2022-06-05 02:11:57 -0300

    Crie o componente Footer e seu CSS

---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
+function Footer(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Footer;
