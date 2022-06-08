6117e151f7873130ca8883cdc9e0be064f1ddd83
felipenascto7 <96033648+felipenascto7@users.noreply.github.com>
2022-06-08 10:08:47 -0300

    Adicionando página catálogo e estilização catálogo

---
+++ Catalogo.jsx
@@ -0,0 +1,57 @@
+import axios from "axios";
+import React, { useEffect, useRef, useState } from "react";
+import FilmeCard from "../componentes/FilmeCard.jsx";
+import './Catalogo.css'
+
+function Catalogo() {
+  // const API_URL = "https://cinema--api.herokuapp.com/filmes";
+  const API_URL = 'http://localhost:3001/filmes'
+
+  const [filmes, setFilmes] = useState([]);
+  const [value, setValue] = useState("");
+
+  const getFilmes = () => {
+    axios
+      .get(API_URL)
+      .then((res) => setFilmes(res.data.mensagem.filme))
+      .catch((erro) => console.log(erro));
+  };
+
+  const getFilmesPorId = id => {
+    axios.get(`${API_URL}/${id}`)
+         .then(res => setFilmes(res.data.mensagem.filme))
+         .catch(erro => console.log(erro));
+  }
+
+  useEffect(() => {
+    getFilmes();
+  }, []);
+
+  return (
+    <div className="catalogo">
+      <div className="container">
+      </div>      
+      <div className="pesquisa">
+        <div className="pesquisa-input">
+        <input
+          type="text"
+          placeholder="Qual filme deseja buscar?"
+          className="campo-de-busca"
+          value={value}
+          onChange={e => {setValue(e.target.value)}}
+        />
+        </div>
+        <button className="bnt" 
+        onClick={e => {e.preventDefault()
+          getFilmesPorId(value)}}>pesquisar</button>
+      </div>
+      <div className="lista-de-filmes">
+        {filmes.map((filme) => {
+          return <FilmeCard key={filme.nome} filme={filme} />;
+        })} 
+      </div>
+    </div>
+  );
+}
+
+export default Catalogo;
